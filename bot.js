9dff4a2840c78f1768626e7c77f7f69d5c43443a
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-12-12 11:30:54 -0800

    indent using spaces (new editor setup messed it up)

--- bot.js
+++ bot.js
@@ -1,27 +1,27 @@
 var irc = require('irc')
 
 var client = new irc.Client('irc.freenode.net', 'WraithBot', {
-	channels: ['#pdxbots']
+    channels: ['#pdxbots']
 })
 
 // Logging to console
 client.addListener('message', function(from, to, message) {
-	console.log(from + ' said ' + message + ' to ' + to)
+    console.log(from + ' said ' + message + ' to ' + to)
 })
 
 // Reply to ping
 client.addListener('message', function(from, to, message) {
-	console.log(from + ' said ' + message + ' to ' + to)
-	if (/^ping$/.test(message)) {
-		client.say(to, 'pong')
-	}
+    console.log(from + ' said ' + message + ' to ' + to)
+    if (/^ping$/.test(message)) {
+        client.say(to, 'pong')
+    }
 })
 
 // Shout at people
 client.addListener('message', function(from, to, message) {
-	console.log(from + ' said ' + message + ' to ' + to)
-	var result = /^!shout (.*)$/.exec(message)
-	if (result) {
-		client.say(to, result[1].toUpperCase())
-	}
+    console.log(from + ' said ' + message + ' to ' + to)
+    var result = /^!shout (.*)$/.exec(message)
+    if (result) {
+        client.say(to, result[1].toUpperCase())
+    }
 })
